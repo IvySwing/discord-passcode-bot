aeb90afcaee110ee36206ee616329f297161cc3d
IvySwing <entense@att.net>
2023-11-20 21:24:25 +0000

    set up create puzzle

---
+++ commands/test.ts
@@ -0,0 +1,30 @@
+import { CommandInteraction, SlashCommandBuilder } from 'discord.js'
+import { Interaction } from 'discord.js'
+
+export const data = new SlashCommandBuilder()
+		.setName('create')
+		.setDescription('create your puzzle')
+		.addStringOption(option =>
+			option.setName('passcode')
+				.setDescription('enter in passcode')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('text')
+				.setDescription('enter in quest text')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('title')
+				.setDescription('name of puzzle')
+				.setRequired(true))
+
+export async function execute(interaction: CommandInteraction){
+	// if we're not repliable then exit
+	if (!interaction.isRepliable()) {
+		return
+	}
+	await interaction.reply({
+		content: `you're all set`,
+		fetchReply: true,
+	})
+	// console.log('interaction', interaction.options.get('passcode'))
+};
